9c4dda3d2b32491b9e462d30708b04c9216e8e33
Vivek Mane <vivekmane172@gmail.com>
2026-01-02 11:55:22 +0530

    Conditional rendering

--- Day 01/vite-project-2/src/components/ListGroup.tsx
+++ Day 01/vite-project-2/src/components/ListGroup.tsx
@@ -1,12 +1,30 @@
 function ListGroup() {
+  let items = ["New York", "San Fransisco", "London", "Paris"];
+  // items = [];
+
+  //   conditional rendering
+  //   if (items.length === 0) {
+  //     return (
+  //       <>
+  //         <h1>List</h1> <p>Items not found.</p>
+  //       </>
+  //     );
+  //   }
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>Items not found </p> : null;
+  // };
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>Items not found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
