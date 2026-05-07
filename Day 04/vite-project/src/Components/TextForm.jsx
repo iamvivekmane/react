2166cc58d0a4a0c9f27f5deceb839234e6d1bd5c
Vivek Mane <vivekmane172@gmail.com>
2026-05-07 09:22:45 +0530

    Bug fixes

--- Day 04/vite-project/src/Components/TextForm.jsx
+++ Day 04/vite-project/src/Components/TextForm.jsx
@@ -45,25 +45,25 @@ const TextForm = (props) => {
 
     return (
         <>
-            <div className='min-vh-100' style={{ backgroundColor: props.textFormColor }}>
-                <div className="container py-2" style={{ backgroundColor: props.textFormColor }}>
+            <div className="min-vh-100" style={{ backgroundColor: props.textFormColor }} >
+                <div className="container">
                     <div className="mb-3">
                         <h1 style={{ color: props.textColor }}>{props.heading}</h1>
-                        <textarea className="form-control" id="myBox" rows="10" style={{ backgroundColor: props.textFormColor }}
+                        <textarea className="form-control" id="myBox" rows="8" style={{ backgroundColor: props.textFormColor }}
                             onChange={handleOnChange} value={text}></textarea>
                     </div>
-                    <button className="btn btn-primary" style={{ backgroundColor: props.buttonColor }}
+                    <button disabled={text.length === 0} className="btn btn-primary" style={{ backgroundColor: props.buttonColor }}
                         onClick={handleUpperClick}>Convert to uppercase</button>
-                    <button className="btn btn-primary mx-3" style={{ backgroundColor: props.buttonColor }} onClick={handleLowerClick}>Convert to lowercase</button>
-                    <button className="btn btn-primary mx-3" style={{ backgroundColor: props.buttonColor }} onClick={handleClearClick}>Clear text</button>
-                    <button className="btn btn-primary mx-3" style={{ backgroundColor: props.buttonColor }} onClick={handleRemoveSpaceClick}>Remove spaces</button>
-                </div>
-                <div className="container my-3" style={{ backgroundColor: props.textFormColor }} style={{ color: props.textColor }} >
-                    <h2>Your text summary</h2>
-                    <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length}  words, {text.trim("").length === 0 ? 0 : text.length} characters</p>
-                    <p>{text.length > 0 ? 0.008 * text.split(" ").length : text.length} Minutes read</p>
-                    <h2>Preview</h2>
-                    <p>{text.split(" ").filter((element) => { return element.length != 0 }).length ? text : 'Enter some text to preview it here'}</p>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1" style={{ backgroundColor: props.buttonColor }} onClick={handleLowerClick}>Convert to lowercase</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1" style={{ backgroundColor: props.buttonColor }} onClick={handleClearClick}>Clear text</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1" style={{ backgroundColor: props.buttonColor }} onClick={handleRemoveSpaceClick}>Remove spaces</button>
+                    <div className="container my-3" style={{ backgroundColor: props.textFormColor }} style={{ color: props.textColor }} >
+                        <h2>Your text summary</h2>
+                        <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length}  words, {text.trim("").length === 0 ? 0 : text.length} characters</p>
+                        <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} Minutes read</p>
+                        <h2>Preview</h2>
+                        <p>{text.split(" ").filter((element) => { return element.length != 0 }).length ? text : 'Enter some text to preview it here'}</p>
+                    </div>
                 </div>
             </div >
         </>
