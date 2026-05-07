9c50b828ed3f72e15c4aee104e44d4bc3a01ae20
Vivek Mane <vivekmane172@gmail.com>
2026-05-07 16:58:03 +0530

    Bug fixes

--- Day 04/vite-project/src/Components/TextForm.jsx
+++ Day 04/vite-project/src/Components/TextForm.jsx
@@ -59,7 +59,7 @@ const TextForm = (props) => {
                     <button disabled={text.length === 0} className="btn btn-primary mx-1" style={{ backgroundColor: props.buttonColor }} onClick={handleRemoveSpaceClick}>Remove spaces</button>
                     <div className="container my-3" style={{ backgroundColor: props.textFormColor }} style={{ color: props.textColor }} >
                         <h2>Your text summary</h2>
-                        <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length}  words, {text.trim("").length === 0 ? 0 : text.length} characters</p>
+                        <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length}  words, {text.trim("").length === 0 ? 0 : text.length} characters</p>
                         <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} Minutes read</p>
                         <h2>Preview</h2>
                         <p>{text.split(" ").filter((element) => { return element.length != 0 }).length ? text : 'Enter some text to preview it here'}</p>
