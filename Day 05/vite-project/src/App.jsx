ffff7dfc6b1a7f2fc1131ebbc5174d98d603258d
Vivek Mane <vivekmane172@gmail.com>
2026-05-08 17:59:24 +0530

    Component structure of app

--- Day 05/vite-project/src/App.jsx
+++ Day 05/vite-project/src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import News from './components/News'
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        My first class based react app also this is not ai generated
+        <Navbar />
+        <News />
       </div>
     )
   }
