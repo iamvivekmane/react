18c8750f25b1ccbe6a1b84224aac7b296cb37bda
Vivek Mane <vivekmane172@gmail.com>
2026-05-08 18:21:00 +0530

    Fetching API key

--- Day 05/vite-project/src/components/News.jsx
+++ Day 05/vite-project/src/components/News.jsx
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is news component
-                <NewsItem />
+            <div className="container my-2">
+                <h2>News: </h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title={"this is title"} description={"This is description"} />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title={"this is title"} description={"This is description"} />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title={"this is title"} description={"This is description"} />
+                    </div>
+                </div>
             </div>
         )
     }
