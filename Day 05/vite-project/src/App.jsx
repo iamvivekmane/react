d7f1f0eba24224ff79ca7be2ef0728a5cda7f4f3
Vivek Mane <vivekmane172@gmail.com>
2026-05-08 17:31:32 +0530

    Class based components setup

---
+++ Day 05/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        My first class based react app also this is not ai generated
+      </div>
+    )
+  }
+}
