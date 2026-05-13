7ae816ed36193d8f3954f74179798f077f7a6cfc
Vivek Mane <vivekmane172@gmail.com>
2026-05-13 10:44:10 +0530

    React router bug fixes in app

--- Day 05/vite-project/src/App.jsx
+++ Day 05/vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import News from './components/News'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
@@ -13,8 +14,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={5} country={'us'} category={'sports'} />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route>
+              <Route exact path="/" element={<News key={'general'}  pageSize={5} country={'us'} category={'general'} />} />
+              <Route exact path="/business" element={<News key={'business'}  pageSize={5} country={'us'} category={'business'} />} />
+              <Route exact path="/entertainment" element={<News  key={'entertainment'} pageSize={5} country={'us'} category={'entertainment'} />} />
+              <Route exact path="/science" element={<News key={'science'}  pageSize={5} country={'us'} category={'science'} />} />
+              <Route exact path="/sports" element={<News  key={'sports'} pageSize={5} country={'us'} category={'sports'} />} />
+              <Route exact path="/technology" element={<News key={'technology'}  pageSize={5} country={'us'} category={'technology'} />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     )
   }
