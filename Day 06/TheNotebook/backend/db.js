20fea61d9a60c59e672c711d721cf26de9ce710f
Vivek Mane <vivekmane172@gmail.com>
2026-05-16 17:55:32 +0530

    Added data validation using express-validator

--- Day 06/TheNotebook/backend/db.js
+++ Day 06/TheNotebook/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoURI = 'mongodb://localhost:27017'
+const mongoURI = 'mongodb://localhost:27017/TheNotebook'
 
 const connectToMongo = async () => {
     await mongoose.connect(mongoURI)
--- Day 06/TheNotebook/backend/routes/auth.js
+++ Day 06/TheNotebook/backend/routes/auth.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body)
-    user.save();
-    res.send(req.body)
+router.post('/', [
+    body('email', 'email is not valid').isEmail(),
+    body('name', 'name is not valid').isLength({ min: 3 }),
+    body('password', 'password must be atleast 5 characters').isLength({ min: 5 }),
+
+], (req, res) => {
+    // console.log(req.body);
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.send(`Helloss, ${req.query.person}!`);
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: 'please enter valid email', err: err.message })
+        })
 })
-
-module.exports = router
+module.exports = router;
