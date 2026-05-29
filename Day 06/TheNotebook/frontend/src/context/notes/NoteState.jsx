93c1ef084e61ce3080a1766486d3469bccb73be8
Vivek Mane <vivekmane172@gmail.com>
2026-05-29 21:33:30 +0530

    Implemented addNote function

--- Day 06/TheNotebook/frontend/src/context/notes/NoteState.jsx
+++ Day 06/TheNotebook/frontend/src/context/notes/NoteState.jsx
@@ -79,25 +79,36 @@ const noteState = (props) => {
 
     }
 
-    //  Edit a note 
-    const editNote = () => {
+    const addNote = async (title, description, tag) => {
+        // TODO: API Call
+        // API Call 
+        const response = await fetch(`${host}/api/notes/addnote`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
+            },
+            body: JSON.stringify({ title, description, tag })
+        });
+        //  Edit a note 
+        const editNote = () => {
 
+        }
+
+        //  Delete a note 
+        const deleteNote = () => {
+
+        }
+
+        const
+        return (
+            <NoteContext.Provider value={{ notes, addNote, editNote, deleteNote }}>
+                {props.children}
+            </NoteContext.Provider>
+
+
+        )
     }
 
-    //  Delete a note 
-    const deleteNote = () => {
 
-    }
-
-    const
-    return (
-        <NoteContext.Provider value={{ notes, addNote, editNote, deleteNote }}>
-            {props.children}
-        </NoteContext.Provider>
-
-
-    )
-}
-
-
-export default noteState;
+    export default noteState;
