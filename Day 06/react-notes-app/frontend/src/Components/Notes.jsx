1726f2e0fc7db97e6166817e0a1e755dd4b0fa02
Vivek Mane <vivekmane172@gmail.com>
2026-06-07 08:26:57 +0530

    Implemented editNote functionality using modal

--- Day 06/react-notes-app/frontend/src/Components/Notes.jsx
+++ Day 06/react-notes-app/frontend/src/Components/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import NoteContext from "../context/notes/noteContext"
 import Noteitem from './Noteitem';
 import AddNote from './addNote';
@@ -8,14 +8,27 @@ const Notes = () => {
     useEffect(() => {
         getNotes();
     }, [])
+    const [note, setNote] = useState({ etitle: "", edescription: "", etag: "" })
+
     const ref = useRef(null)
 
-    const updateNote = (note) => {
+    const updateNote = (currentNote) => {
         ref.current.click();
+        setNote({ etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+    }
+
+    const handleClick = (e) => {
+        console.log("handleCLick called", note)
+        e.preventDefault();
+    }
+
+    const onChange = (e) => {
+        console.log("On change is called")
+        setNote({ ...note, [e.target.name]: e.target.value })
     }
     return (
         <>
-            <button ref={ref} type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+            <button ref={ref} style={{ display: 'none' }} type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >
                 Edit note
             </button>
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -26,11 +39,24 @@ const Notes = () => {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <form className='my-3'>
+                                <div className="mb-3">
+                                    <label htmlFor="title" className="form-label">Title</label>
+                                    <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange} value={note.etitle} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange} value={note.edescription} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="tag" className="form-label">Tag</label>
+                                    <input type="text" className="form-control" id="etag" name="etag" onChange={onChange} value={note.etag} />
+                                </div>
+                            </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="button" class="btn btn-primary" onClick={handleClick}>Update note</button>
                         </div>
                     </div>
                 </div>
