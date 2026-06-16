ba990d1d25af02bcc52d75b293fe788d577165fb
Vivek Mane <vivekmane172@gmail.com>
2026-06-16 21:08:45 +0530

    Added heading to components

--- Day 06/react-notes-app/frontend/src/Components/Navbar.jsx
+++ Day 06/react-notes-app/frontend/src/Components/Navbar.jsx
@@ -33,7 +33,11 @@ const Navbar = () => {
                         {!localStorage.getItem('token') ? <form className='d-flex'>
                             <Link className="btn btn-primary mx-1" type="submit" to="/Login">Login</Link>
                             <Link className="btn btn-primary mx-1" type="submit" to="/Signup">Signup</Link>
-                        </form> : <button className="btn btn-primary mx-1" type="submit" onClick={handleLogout} >Logout</button>}
+                        </form> : <div>
+                            <span className='mx-2'></span>
+                            <button className="btn btn-primary mx-1" type="submit" onClick={handleLogout} >Logout</button>
+                        </div>}
+
 
                     </div>
                 </div>
