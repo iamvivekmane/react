077317c585f881ea577b14cd8e8ac178f7f70800
Vivek Mane <vivekmane172@gmail.com>
2026-06-18 16:42:29 +0530

    Implemented new UI for login component

--- Day 06/react-notes-app/frontend/src/Components/Login.jsx
+++ Day 06/react-notes-app/frontend/src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 
 const Login = (props) => {
     const [credentials, setCredentials] = useState({ email: "", password: "" })
@@ -32,21 +32,25 @@ const Login = (props) => {
         setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
     return (
-        <form onSubmit={handleSubmit} >
-            <div className="heading d-flex justify-content-center align-items-center">
-                <h1 className='center'>Login</h1>
-            </div>
-            <div className="mb-3" >
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange} />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} />
-            </div>
-            <button type="submit" className="btn btn-primary" >Login</button>
-        </form >
+        <>
+            <form onSubmit={handleSubmit} >
+                <div className="heading d-flex justify-content-center align-items-center ">
+                    <h1 className='center'>Login</h1>
+                </div>
+                <div className="main d-flex align-items-center justify-content-center flex-column">
+                    <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                        <label htmlFor="email" className="form-label text-start">Email</label>
+                        <input type="email" className="form-control w-100" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange} />
+                    </div>
+                    <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control w-100" id="password" name="password" value={credentials.password} onChange={onChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-50 max-sm:w-100" >Login</button>
+                    <Link type="button" className="btn border  w-50 m-2" to="/Signup" >Signup</Link>
+                </div>
+            </form >
+        </>
     )
 }
 
