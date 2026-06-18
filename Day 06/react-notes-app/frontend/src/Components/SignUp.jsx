61bbc28198526194be68957e27e3ebd7dfe0b600
Vivek Mane <vivekmane172@gmail.com>
2026-06-18 16:42:57 +0530

    Implemented new UI for signup component

--- Day 06/react-notes-app/frontend/src/Components/SignUp.jsx
+++ Day 06/react-notes-app/frontend/src/Components/SignUp.jsx
@@ -34,27 +34,29 @@ const Signup = (props) => {
     return (
 
         <form onSubmit={handleSubmit} >
-            <div className="heading d-flex justify-content-center align-items-center">
+            <div className="heading d-flex justify-content-center align-items-center flex-column">
                 <h1 className='center'>Signup</h1>
             </div>
-            <div className="mb-3" >
-                <label htmlFor="name" className="form-label">Name</label>
-                <input type="name" className="form-control" id="name" name="name" aria-describedby="emailHelp" value={credentials.name} onChange={onChange} required />
-            </div>
-            <div className="mb-3" >
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange} required />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" name="password" onChange={onChange} required minLength={5} />
-            </div>
+            <div className="main d-flex align-items-center justify-content-center flex-column">
 
-            <div className="mb-3">
-                <label htmlFor="cpassword" className="form-label">Confirm password</label>
-                <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} required minLength={5} />
+                <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="name" className="form-control" id="name" name="name" aria-describedby="emailHelp" value={credentials.name} onChange={onChange} required />
+                </div>
+                <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange} required />
+                </div>
+                <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" onChange={onChange} required minLength={5} />
+                </div>
+                <div className="mb-3 d-flex w-50 justify-content-center align-itms-center flex-column " >
+                    <label htmlFor="cpassword" className="form-label">Confirm password</label>
+                    <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} required minLength={5} />
+                </div>
+                <button type="submit" className="btn btn-primary w-50">Signup</button>
             </div>
-            <button type="submit" className="btn btn-primary">Signup</button>
         </form >
     )
 }
