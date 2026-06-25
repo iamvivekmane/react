35b153b4ad38353355c32561d2fe43b2e8668ea1
Vivek Mane <vivekmane172@gmail.com>
2026-06-25 17:24:29 +0530

    Updated state using dispatch function

--- Day 08/react-redux/src/Shop.jsx
+++ Day 08/react-redux/src/Shop.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state';
+
 
 const Shop = () => {
+    const dispatch = useDispatch();
     return (
         <>
+
             <div className="container">
                 <h2 className='mx-2'>Deposit/Withdraw money</h2>
-                <button className='btn btn-primary mx-2'>-</button>
+                <button className='btn btn-primary mx-2' onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }}>-</button>
                 Update balance
-                <button className='btn btn-primary mx-2'>+</button>
+                <button className='btn btn-primary mx-2' onClick={() => { dispatch(actionCreators.depositMoney(100)) }}>+</button>
             </div>
 
         </>
