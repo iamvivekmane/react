41ac38c9f1ba971e7da3154b4c2232b6688ef27b
Vivek Mane <vivekmane172@gmail.com>
2026-06-25 10:27:32 +0530

    React redux setup

---
+++ Day 08/react-redux/src/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <>
+            <div className="container">
+                <h2 className='mx-2'>Buy bread</h2>
+                <button className='btn btn-primary mx-2'>-</button>
+                Add to cart
+                <button className='btn btn-primary mx-2'>+</button>
+            </div>
+
+        </>
+
+    )
+}
+
+export default Shop
