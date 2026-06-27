f77fb6e09abe6b0da3f8882f2edd7446a7fdc63f
Vivek Mane <vivekmane172@gmail.com>
2026-06-27 10:13:53 +0530

    Added strikethrough tasks functionality

--- React practice/todo/src/components/Task.jsx
+++ React practice/todo/src/components/Task.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Task = (props) => {
     const TasksLocal = props.Tasks;
@@ -10,15 +10,28 @@ const Task = (props) => {
     // const deleteButton = () => {
     //     props.setCurrentIndex(TasksLocal.id);
     // }
+    const [flag, setFlag] = useState(false)
+
+    const onRadioClick = () => {
+        if (flag) {
+            setFlag(false);
+        }
+        else {
+            setFlag(true)
+        }
+        console.log("clicked me")
+    }
     return (
         <div className="form-check d-flex justify-content-between gap-5 border px-4 py-2 m-3">
             <div>
-                <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" />
+                <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" onClick={onRadioClick} />
             </div>
             <div>
-                <label className="form-check-label" htmlFor="radioDefault1">
+                {flag ? <label className="form-check-label" htmlFor="radioDefault1">
+                    <s>{TasksLocal.title}</s>
+                </label> : <label className="form-check-label" htmlFor="radioDefault1">
                     {TasksLocal.title}
-                </label>
+                </label>}
             </div>
             <div>
                 <button onClick={() => { props.onDelete(TasksLocal.id) }}>Delete</button>
